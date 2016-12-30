e242ba2381c6c17bfe5b291041d83a16e68378a5
wiese <pablo@grass.digital>
2016-12-30 23:43:01 +0100

    first steps in compound before and after

--- src/OptionFilter/Incarnation/Compound.php
+++ src/OptionFilter/Incarnation/Compound.php
@@ -3,7 +3,10 @@
 namespace wiese\ApproximateDateTime\OptionFilter\Incarnation;
 
 use wiese\ApproximateDateTime\Config;
+use wiese\ApproximateDateTime\Clue;
+use wiese\ApproximateDateTime\DateTimeData;
 use wiese\ApproximateDateTime\OptionFilter\Base;
+use wiese\ApproximateDateTime\Range;
 use wiese\ApproximateDateTime\Ranges;
 
 class Compound extends Base
@@ -24,7 +27,98 @@ class Compound extends Base
 //            return $ranges;
 //        }
 
+        $processUnits = explode('-', $this->unit);
+
+        foreach ($this->clues as $clue) {
+            /**
+             * @var $clue Clue
+             */
+
+            $units = $clue->getSetUnits();
+            if (count($units) < 2) {
+                continue;
+            }
+
+            if ($processUnits !== $units) {
+                continue;
+            }
+
+            if ($clue->filter === Clue::FILTER_BEFOREEQUALS) {
+                $ranges = $this->applyBefore($ranges, $clue);
+            } elseif ($clue->filter === Clue::FILTER_AFTEREQUALS) {
+                $ranges = $this->applyAfter($ranges, $clue);
+            }
+
+            // before
+            // after
+
+
+
+            // blacklist
+            // whitelist
+        }
+
         return $ranges;
+    }
+
+    public function applyAfter(Ranges $ranges, Clue $clue): Ranges
+    {
+        $newRanges = new Ranges();
+
+        foreach ($ranges as $range) {
+            /**
+             * @var $range Range
+             */
+            if ($range->getStart()->isBigger($clue)) {   // completely later
+                $newRanges->append($range);
+                continue;
+            } elseif ($range->getEnd()->isSmaller($clue)) {   // completely earlier
+                break;  // ignore all following, as ranges should be in order
+            } else {  // start earlier, but end later, i.e. overlapping
+                // ignore
+                $newRange = clone $range;
+                $start = $range->getStart();
+                foreach ($clue->getSetUnits() as $unit) {
+                    $start->set($unit, $clue->get($unit));
+                }
+                $newRange->setStart($start);
+                $newRanges->append($newRange);
+
+                break;
+            }
+        }
+
+        return $newRanges;
+    }
+
+    public function applyBefore(Ranges $ranges, Clue $clue): Ranges
+    {
+        $newRanges = new Ranges();
+
+        foreach ($ranges as $range) {
+            /**
+             * @var $range Range
+             */
+            if ($range->getEnd()->isSmaller($clue)) {
+                $newRanges->append($range);
+                continue;
+            } elseif ($range->getStart()->isBigger($clue)) {
+                break;  // ignore all following, as ranges should be in order
+            } else {  // start earlier, but end later, i.e. overlapping
+                // ignore
+                $newRange = clone $range;
+                $end = $range->getEnd();
+                foreach ($clue->getSetUnits() as $unit) {
+                    $end->set($unit, $clue->get($unit));
+                }
+                $newRange->setEnd($end);
+                $newRanges->append($newRange);
+
+                break;
+            }
+        }
+
+        return $newRanges;
     }
 
     protected function getCompoundUnits() : array
--- tests/OptionFilter/Incarnation/CompoundTest.php
+++ tests/OptionFilter/Incarnation/CompoundTest.php
@@ -2,6 +2,7 @@
 
 namespace wiese\ApproximateDateTime\Tests\OptionFilter\Incarnation;
 
+use wiese\ApproximateDateTime\Clues;
 use wiese\ApproximateDateTime\Tests\OptionFilter\ParentTest;
 use wiese\ApproximateDateTime\Clue;
 use wiese\ApproximateDateTime\DateTimeData;
@@ -31,18 +32,10 @@ class CompoundTest extends ParentTest
         $this->sut->setUnit($unit);
         $this->sut->setTimezone($this->tz);
         $this->sut->setCalendar(CAL_GREGORIAN);
-        // keep a reference for modification during individual tests
-        $this->clues = $this->getMockBuilder('wiese\ApproximateDateTime\Clues')
-            // methods that are mocked; results can be manipulated later
-            ->setMethods(['getWhitelist', 'getBlacklist', 'getAfter', 'getBefore'])
-            ->getMock();
-        $this->sut->setClues($this->clues);
     }
 
-    public function testYearMonth() : void
+    public function testAfter() : void
     {
-        $this->markTestIncomplete();
-
         $this->init('y-m');
 
         $ranges = new Ranges();
@@ -57,19 +50,106 @@ class CompoundTest extends ParentTest
         $range->setEnd($end);
         $ranges->append($range);
 
+        $clues = new Clues();
+
         $clue = new Clue();
         $clue->setY(1994);
         $clue->setM(6);
         $clue->filter = Clue::FILTER_AFTEREQUALS;
-        $this->mockClues($clue, null, [], []);
+        $clues->append($clue);
+
+        $this->sut->setClues($clues);
 
         $ranges = $this->sut->apply($ranges);
 
         $this->assertEquals(1994, $ranges[0]->getStart()->getY());
         $this->assertEquals(6, $ranges[0]->getStart()->getM());
-        $this->assertEquals(1, $ranges[0]->getStart()->getD());
         $this->assertEquals(1995, $ranges[0]->getEnd()->getY());
         $this->assertEquals(11, $ranges[0]->getEnd()->getM());
-        $this->assertEquals(30, $ranges[0]->getEnd()->getD());
+    }
+
+    public function testBefore() : void
+    {
+        $this->init('y-m-d');
+
+        $ranges = new Ranges();
+        $range = new Range();
+        $start = new DateTimeData($this->tz);
+        $start->setY(1994);
+        $start->setM(1);
+        $start->setD(3);
+        $range->setStart($start);
+        $end = new DateTimeData($this->tz);
+        $end->setY(1995);
+        $end->setM(11);
+        $end->setM(19);
+        $range->setEnd($end);
+        $ranges->append($range);
+
+        $clues = new Clues();
+
+        $clue = new Clue();
+        $clue->setY(1995);
+        $clue->setM(10);
+        $clue->setD(3);
+        $clue->filter = Clue::FILTER_BEFOREEQUALS;
+        $clues->append($clue);
+
+        $this->sut->setClues($clues);
+
+        $ranges = $this->sut->apply($ranges);
+
+        $this->assertEquals(1994, $ranges[0]->getStart()->getY());
+        $this->assertEquals(1, $ranges[0]->getStart()->getM());
+        $this->assertEquals(3, $ranges[0]->getStart()->getD());
+        $this->assertEquals(1995, $ranges[0]->getEnd()->getY());
+        $this->assertEquals(10, $ranges[0]->getEnd()->getM());
+        $this->assertEquals(3, $ranges[0]->getEnd()->getD());
+    }
+
+    public function testBeforeAndAfter() : void
+    {
+        $this->init('y-m-d');
+
+        $ranges = new Ranges();
+        $range = new Range();
+        $start = new DateTimeData($this->tz);
+        $start->setY(1975);
+        $start->setM(9);
+        $start->setD(1);
+        $range->setStart($start);
+        $end = new DateTimeData($this->tz);
+        $end->setY(1975);
+        $end->setM(9);
+        $end->setM(30);
+        $range->setEnd($end);
+        $ranges->append($range);
+
+        $clues = new Clues();
+
+        $clue = new Clue();
+        $clue->setY(1975);
+        $clue->setM(9);
+        $clue->setD(24);
+        $clue->filter = Clue::FILTER_AFTEREQUALS;
+        $clues->append($clue);
+
+        $clue = new Clue();
+        $clue->setY(1975);
+        $clue->setM(9);
+        $clue->setD(26);
+        $clue->filter = Clue::FILTER_BEFOREEQUALS;
+        $clues->append($clue);
+
+        $this->sut->setClues($clues);
+
+        $ranges = $this->sut->apply($ranges);
+
+        $this->assertEquals(1975, $ranges[0]->getStart()->getY());
+        $this->assertEquals(9, $ranges[0]->getStart()->getM());
+        $this->assertEquals(24, $ranges[0]->getStart()->getD());
+        $this->assertEquals(1975, $ranges[0]->getEnd()->getY());
+        $this->assertEquals(9, $ranges[0]->getEnd()->getM());
+        $this->assertEquals(26, $ranges[0]->getEnd()->getD());
     }
 }
